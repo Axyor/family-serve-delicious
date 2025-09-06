7e1f965ee62192f418185c6ad5718c0eff58cadc
Alexandre ROY <66955818+Axyor@users.noreply.github.com>
2025-09-06 22:39:30 +0200

    Feat/update tools (#4)
    * feat: reorganize readme
    
    * feat: addapte group tools
    
    * feat: clear tools and add and addapt them
    
    * feat: update new tool and implement new db import rules

--- jest.config.js
+++ jest.config.js
@@ -4,14 +4,6 @@ module.exports = {
   testEnvironment: 'node',
   roots: ['<rootDir>/tests'],
   moduleFileExtensions: ['ts', 'js', 'json'],
-  moduleNameMapper: {
-    '^\\./db\\.js$': '<rootDir>/src/db.ts',
-    '^\\./resources/group\\.resource\\.js$': '<rootDir>/src/resources/group.resource.ts',
-    '^\\./tools/group\\.tools\\.js$': '<rootDir>/src/tools/group.tools.ts',
-    '^\.\./db\\.js$': '<rootDir>/src/db.ts',
-    // Any other src-relative .js import -> .ts (does not match node_modules)
-    '^((?:\\.{1,2})/src/.*)\\.js$': '$1.ts'
-  },
   transform: {
     '^.+\\.(ts|tsx)$': [ 'ts-jest', { tsconfig: { target: 'es2023', module: 'Node16' } } ]
   },
